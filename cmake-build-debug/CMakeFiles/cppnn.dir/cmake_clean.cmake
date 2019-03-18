4601e8fc8003fb4999a5f8ea1715cd232bbc6e90
zappyfish <liamkelly2015@gmail.com>
2019-03-18 01:12:24 -0500

    updates

---
+++ cmake-build-debug/CMakeFiles/cppnn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppnn.dir/main.cpp.o"
+  "cppnn.pdb"
+  "cppnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cppnn.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cppnn.
+# This may be replaced when dependencies are built.
