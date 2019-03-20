9ac4fb7f6abfeb1a89d1fe2407394185dd91a6de
zappyfish <liamkelly2015@gmail.com>
2019-03-19 21:07:02 -0500

    debug

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ typedef std::vector<std::pair<std::string, tensorflow::Tensor>> tensor_dict;
 
 cv::Mat loadImageFloat(std::string &path);
 Tensor getTensor(cv::Mat &inp1, cv::Mat &inp2, cv::Mat &inp3);
-void fillTensor(cv::Mat &src, Tensor &tensor, int startCol);
+void fillTensor(cv::Mat &src, Tensor &tensor, int start);
 
 int main() {
     // set up your input paths
@@ -93,24 +93,24 @@ int main() {
 Tensor getTensor(cv::Mat &inp1, cv::Mat &inp2, cv::Mat &inp3) {
 
     tensorflow::Tensor input_tensor(tensorflow::DT_FLOAT,
-                                tensorflow::TensorShape({1, inp1.rows, inp1.cols * 3, 3}));
+                                tensorflow::TensorShape({1, inp1.rows, inp1.cols, 9}));
 
     fillTensor(inp1, input_tensor, 0);
-    fillTensor(inp2, input_tensor, inp1.cols);
-    fillTensor(inp3, input_tensor, 2 * inp1.cols);
+    fillTensor(inp2, input_tensor, 3);
+    fillTensor(inp3, input_tensor, 6);
 
     return input_tensor;
 }
 
-void fillTensor(cv::Mat &src, Tensor &tensor, int startCol) {
+void fillTensor(cv::Mat &src, Tensor &tensor, int start) {
     auto input_tensor_mapped = tensor.tensor<float, 4>();
     for (int row = 0; row < src.rows; row++) {
         for (int col = 0; col < src.cols; col++) {
             cv::Vec3b pixel = src.at<cv::Vec3b>(row, col);
             // TODO: check if it should be 0, 1, 2 pixel order instead
-            input_tensor_mapped(0, row, col + startCol, 0) = pixel[2];
-            input_tensor_mapped(0, row, col + startCol, 1) = pixel[1];
-            input_tensor_mapped(0, row, col + startCol, 2) = pixel[0];
+            input_tensor_mapped(0, row, col, 0 + start) = pixel[2];
+            input_tensor_mapped(0, row, col, 1 + start) = pixel[1];
+            input_tensor_mapped(0, row, col, 2 + start) = pixel[0];
 
         }
     }
