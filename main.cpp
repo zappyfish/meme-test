25e54df9052097506d58a7e0e8ef6013dc983adf
zappyfish <liamkelly2015@gmail.com>
2019-03-19 22:18:03 -0500

    debug

--- main.cpp
+++ main.cpp
@@ -83,6 +83,7 @@ int main() {
             std::cout << "feeded\n";
             imgs[0] = imgs[1];
             imgs[1] = imgs[2]; // shift over
+            std::cout << outputTensors.size() << std::endl;
             std::cout << outputTensors[0].DebugString() << std::endl;
         }
     }
