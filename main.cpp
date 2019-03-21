57ba44abe0c9da050dac89ff94649ee484487196
zappyfish <liamkelly2015@gmail.com>
2019-03-20 23:29:14 -0500

    pls work

--- main.cpp
+++ main.cpp
@@ -92,7 +92,7 @@ int main() {
         if (num_images == 3) {
             Tensor imgTensor = getInputTensor(imgs[0], imgs[1], imgs[2]);
             tensor_dict feedDict = {
-                    {"truediv_1", imgTensor}
+                    {"input", imgTensor}
             };
             std::cout << "feed me\n";
             std::vector<tensorflow::Tensor> outputTensors;
